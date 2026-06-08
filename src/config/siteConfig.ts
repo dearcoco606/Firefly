53d1e19c99c903576d4af517c2c0b7708e7249d2
dearcoco606 <hongwoo6@163.com>
2026-06-08 15:43:12 +0200

    first deploy

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,10 +7,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "Mayrain",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "Mayrain's Blog",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
@@ -21,7 +21,7 @@ export const siteConfig: SiteConfig = {
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
+		"Mayrain",
 		"Fuwari",
 		"Astro",
 		"ACGN",
@@ -91,7 +91,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-06-08",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
